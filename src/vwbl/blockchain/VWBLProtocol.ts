4ab72174edd0e6b4361b9e1a05da9affe21adbb3
seki <schokolade0317@gmail.com>
2022-03-01 15:44:37 +0900

    return owner

--- src/vwbl/blockchain/VWBLProtocol.ts
+++ src/vwbl/blockchain/VWBLProtocol.ts
@@ -36,9 +36,13 @@ export class VWBLNFT {
     return await this.contract.methods.tokenURI(tokenId).call();
   };
 
+  getOwner = async (tokenId: number) => {
+    return await this.contract.methods.ownerOf(tokenId).call();
+  };
+
   isOwnerOf = async (tokenId: number) => {
     const myAddress = (await this.web3.eth.getAccounts())[0];
-    const owner = await this.contract.methods.ownerOf(tokenId).call();
+    const owner = await this.getOwner(tokenId);
     return myAddress === owner;
   };
 }
