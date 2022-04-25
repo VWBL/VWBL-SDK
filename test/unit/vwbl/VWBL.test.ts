c9d30b614ab65a7596ff3a2e9c98518e0fca5b1e
Taki <hashimoto19980924@gmail.com>
2022-04-25 18:40:26 +0900

    add test

--- test/unit/vwbl/VWBL.test.ts
+++ test/unit/vwbl/VWBL.test.ts
@@ -23,6 +23,7 @@ describe("VWBL",  () => {
 
   const vwbl = new VWBL({
     awsConfig: undefined,
+    chainId: 3,
     contractAddress: "0x2c7e967093d7fe0eeb5440bf49e5D148417B0412",
     manageKeyType: ManageKeyType.VWBL_NETWORK_SERVER,
     uploadContentType: UploadContentType.CUSTOM,
