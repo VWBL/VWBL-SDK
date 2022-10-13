0e40d7b4edbf5ae1ff1634a34c2ed6221e081d97
Asuma Yamada <yamadaasuma@gmail.com>
2022-10-14 00:23:10 +0900

    fix: add files for ERC1155

---
+++ src/vwbl/blockchain/ERC1155/VWBLProtocol.ts
@@ -0,0 +1,129 @@
+import Web3 from "web3";
+import { Contract } from "web3-eth-contract";
+import { AbiItem } from "web3-utils";
+
+import vwbl1155 from "../../../contract/VWBLERC1155.json";
+import vwbl1155IPFS from "../../../contract/VWBLERC1155.json";
+
+export class VWBLNFT {
+  private contract: Contract;
+  private web3: Web3;
+
+  constructor(web3: Web3, address: string, isIpfs: boolean) {
+    this.web3 = web3;
+    this.contract = isIpfs
+      ? new web3.eth.Contract(vwbl1155IPFS.abi as AbiItem[], address)
+      : new web3.eth.Contract(vwbl1155.abi as AbiItem[], address);
+  }
+
+  async mintToken(decryptUrl: string, amount: number, royaltiesPercentage: number, documentId: string) {
+    const myAddress = (await this.web3.eth.getAccounts())[0];
+    const fee = await this.getFee();
+    console.log("transaction start");
+    const receipt = await this.contract.methods.mint(decryptUrl, amount, royaltiesPercentage, documentId).send({
+      from: myAddress,
+      value: fee,
+      maxPriorityFeePerGas: null,
+      maxFeePerGas: null,
+    });
+    console.log("transaction end");
+    const tokenId: number = receipt.events.Transfer.returnValues.tokenId;
+    return tokenId;
+  }
+
+  async mintTokenForIPFS(
+    metadataUrl: string,
+    decryptUrl: string,
+    amount: number,
+    royaltiesPercentage: number,
+    documentId: string
+  ) {
+    const myAddress = (await this.web3.eth.getAccounts())[0];
+    const fee = await this.getFee();
+    console.log("transaction start");
+    const receipt = await this.contract.methods
+      .mint(metadataUrl, decryptUrl, amount, royaltiesPercentage, documentId)
+      .send({
+        from: myAddress,
+        value: fee,
+        maxPriorityFeePerGas: null,
+        maxFeePerGas: null,
+      });
+    console.log("transaction end");
+    const tokenId: number = receipt.events.Transfer.returnValues.tokenId;
+    return tokenId;
+  }
+
+  async getOwnTokenIds() {
+    const myAddress = (await this.web3.eth.getAccounts())[0];
+    const balance = await this.contract.methods.tokenCountOfOwner(myAddress).call();
+    return await Promise.all(
+      range(Number.parseInt(balance)).map(async (i) => {
+        const ownTokenId = await this.contract.methods.tokenOfOwnerByIndex(myAddress, i).call();
+        return Number.parseInt(ownTokenId);
+      })
+    );
+  }
+
+  async getTokenByMinter(address: string) {
+    return await this.contract.methods.getTokenByMinter(address).call();
+  }
+
+  async getMetadataUrl(tokenId: number) {
+    return await this.contract.methods.tokenURI(tokenId).call();
+  }
+
+  async getOwner(tokenId: number) {
+    return await this.contract.methods.ownerOf(tokenId).call();
+  }
+
+  async getMinter(tokenId: number) {
+    return await this.contract.methods.getMinter(tokenId).call();
+  }
+
+  async isOwnerOf(tokenId: number) {
+    const myAddress = (await this.web3.eth.getAccounts())[0];
+    const owner = await this.getOwner(tokenId);
+    return myAddress === owner;
+  }
+
+  async isMinterOf(tokenId: number) {
+    const myAddress = (await this.web3.eth.getAccounts())[0];
+    const minter = await this.getMinter(tokenId);
+    return myAddress === minter;
+  }
+
+  async getFee() {
+    return await this.contract.methods.getFee().call();
+  }
+
+  async getTokenInfo(tokenId: number) {
+    return await this.contract.methods.tokenIdToTokenInfo(tokenId).call();
+  }
+
+  async setApprovalForAll(operator: string): Promise<void> {
+    const myAddress = (await this.web3.eth.getAccounts())[0];
+    await this.contract.methods.setApprovalForAll(operator, true).send({
+      from: myAddress,
+      maxPriorityFeePerGas: null,
+      maxFeePerGas: null,
+    });
+  }
+
+  async isApprovedForAll(owner: string, operator: string): Promise<boolean> {
+    return await this.contract.methods.isApprovedForAll(owner, operator).call();
+  }
+
+  async safeTransfer(to: string, tokenId: number, amount: number): Promise<void> {
+    const myAddress = (await this.web3.eth.getAccounts())[0];
+    await this.contract.methods.safeTransferFrom(myAddress, to, tokenId, amount).send({
+      from: myAddress,
+      maxPriorityFeePerGas: null,
+      maxFeePerGas: null,
+    });
+  }
+}
+
+const range = (length: number) => {
+  return Array.from(Array(length).keys());
+};
