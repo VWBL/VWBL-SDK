0dd06de3498d62fd45497460a26c7961af020216
Asuma Yamada <yamadaasuma@gmail.com>
2022-10-20 21:09:03 +0900

    fix: test

--- test/unit/vwbl/VWBL.test.ts
+++ test/unit/vwbl/VWBL.test.ts
@@ -1,64 +1,96 @@
+import { expect } from "chai";
+import * as dotenv from "dotenv";
+import sinon from "sinon";
+import Web3 from "web3";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const FileAPI = require("file-api"),
+  File = FileAPI.File;
+
 import {
-  FileType,
   ManageKeyType,
   UploadContentType,
   UploadMetadataType,
   VWBL,
   VWBLApi,
-  VWBLNFT
+  VWBLNFT,
 } from "../../../src/vwbl";
-import Web3 from "web3";
-import sinon from "sinon"
-import { expect } from "chai";
-import * as dotenv from "dotenv";
 
 dotenv.config();
 
-describe("VWBL",  () => {
-  const providerUrl = process.env.PROVIDER_URL;
-  const web3 = new Web3(providerUrl as string);
+const vwblApiStub = {
+  setKey: sinon.stub(VWBLApi.prototype, "setKey"),
+};
+
+const providerUrl = process.env.PROVIDER_URL;
+const web3 = new Web3(providerUrl as string);
+
+sinon.stub(web3.eth, "getAccounts").returns(Promise.resolve(["test address"]));
+sinon.stub(web3.eth.personal, "sign").returns(Promise.resolve("test sign"));
+
+describe("VWBL", () => {
   const vwblProtocolStub = {
     mintToken: sinon.stub(VWBLNFT.prototype, "mintToken"),
   };
 
-  const vwblApiStub = {
-    setKey: sinon.stub(VWBLApi.prototype, "setKey")
-  };
-
   const vwbl = new VWBL({
-    ipfsNftStorageKey: "set nftstorage api key", 
+    ipfsNftStorageKey: "set nftstorage api key",
     awsConfig: undefined,
     contractAddress: "0x2c7e967093d7fe0eeb5440bf49e5D148417B0412",
     manageKeyType: ManageKeyType.VWBL_NETWORK_SERVER,
     uploadContentType: UploadContentType.CUSTOM,
     uploadMetadataType: UploadMetadataType.CUSTOM,
     vwblNetworkUrl: "http://example.com",
-    web3
+    web3,
   });
-  sinon.stub(web3.eth, "getAccounts").returns(Promise.resolve(["test address"]));
-  sinon.stub(web3.eth.personal, "sign").returns(Promise.resolve("test sign"));
+
   before(async () => {
     await vwbl.sign();
   });
+
   it("mint token", async () => {
     vwblProtocolStub.mintToken.returns(Promise.resolve(1));
-    const testFunctions = {uploadFile() {return Promise.resolve({encryptedDataUrl: "https://example.com", thumbnailImageUrl: "https://example.com"})}, async uploadMetadata (){}};
-    const uploadFileStub = sinon.stub(testFunctions, "uploadFile").returns(Promise.resolve({encryptedDataUrl: "https://example.com", thumbnailImageUrl: "https://example.com"}));
+    const testFunctions = {
+      uploadEncryptedFile: () => {
+        return Promise.resolve("https://example.com");
+      },
+      uploadThumbnail: () => {
+        return Promise.resolve("https://example.com");
+      },
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      uploadMetadata: async () => {},
+    };
+    const uploadEncryptedFileStub = sinon
+      .stub(testFunctions, "uploadEncryptedFile")
+      .returns(Promise.resolve("https://example.com"));
+    const uploadFileStub = sinon
+      .stub(testFunctions, "uploadThumbnail")
+      .returns(Promise.resolve("https://example.com"));
     const uploadMetadataStub = sinon.stub(testFunctions, "uploadMetadata");
-    const tokenId = await vwbl.createToken("test token", "test", {
-        name: "plain data",
-        content: "data:image/png;base64,xxx"
-      },
-      FileType.IMAGE, {
+    const tokenId = await vwbl.managedCreateToken(
+      "test token",
+      "test",
+      new File({
         name: "thumbnail image",
-        content: "data:image/png;base64,yyy"
-      },
+        type: "image/png",
+        buffer: Buffer.alloc(100),
+      }),
+      new File({
+        name: "plain data",
+        type: "image/png",
+        buffer: Buffer.alloc(100),
+      }),
       10,
-       testFunctions.uploadFile, testFunctions.uploadMetadata);
+      "base64",
+      testFunctions.uploadEncryptedFile,
+      testFunctions.uploadThumbnail,
+      testFunctions.uploadMetadata
+    );
+
     expect(vwblProtocolStub.mintToken.callCount).equal(1);
     expect(vwblApiStub.setKey.callCount).equal(1);
+    expect(uploadEncryptedFileStub.callCount).equal(1);
     expect(uploadFileStub.callCount).equal(1);
     expect(uploadMetadataStub.callCount).equal(1);
     expect(tokenId).equal(1);
-  })
+  });
 });
