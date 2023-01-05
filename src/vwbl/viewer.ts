ee50846020100b485d14a820a958a4c6f2b92321
bibitto <bibitto.crypto@gmail.com>
2023-01-05 15:35:37 +0900

    update: add owned/minted token metadata getter

--- src/vwbl/viewer.ts
+++ src/vwbl/viewer.ts
@@ -79,6 +79,116 @@ export class VWBLViewer extends VWBLBase {
     return allItems;
   };
 
+  listAllOwnedMetadata = async (userAddress: string): Promise<(ExtendedMetadeta | undefined)[]> => {
+    if (!this.dataCollector) throw new Error("please set dataCollectorAddress");
+    const tokens = await this.dataCollector.methods.getAllOwnedTokens(userAddress).call();
+    const items: (ExtendedMetadeta | undefined)[] = await Promise.all(
+      tokens.map(async (token: TokenInfo) => {
+        const metadata: PlainMetadata = (
+          await axios.get(token.tokenURI, { validateStatus: (status) => status === 200 })
+        ).data;
+        return {
+          id: token.tokenId,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image,
+          mimeType: metadata.mime_type,
+          encryptLogic: metadata.encrypt_logic,
+          address: token.contractAddress,
+        };
+      })
+    );
+    return items;
+  };
+
+  listOwnedNFTMetadata = async (userAddress: string): Promise<(ExtendedMetadeta | undefined)[]> => {
+    if (!this.dataCollector) throw new Error("please set dataCollectorAddress");
+    const tokens = await this.dataCollector.methods.getOwnedNFTs(userAddress).call();
+    const items: (ExtendedMetadeta | undefined)[] = await Promise.all(
+      tokens.map(async (token: TokenInfo) => {
+        const metadata: PlainMetadata = (
+          await axios.get(token.tokenURI, { validateStatus: (status) => status === 200 })
+        ).data;
+        return {
+          id: token.tokenId,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image,
+          mimeType: metadata.mime_type,
+          encryptLogic: metadata.encrypt_logic,
+          address: token.contractAddress,
+        };
+      })
+    );
+    return items;
+  };
+
+  listOwnedERC1155Metadata = async (userAddress: string): Promise<(ExtendedMetadeta | undefined)[]> => {
+    if (!this.dataCollector) throw new Error("please set dataCollectorAddress");
+    const tokens = await this.dataCollector.methods.getOwnedERC1155s(userAddress).call();
+    const items: (ExtendedMetadeta | undefined)[] = await Promise.all(
+      tokens.map(async (token: TokenInfo) => {
+        const metadata: PlainMetadata = (
+          await axios.get(token.tokenURI, { validateStatus: (status) => status === 200 })
+        ).data;
+        return {
+          id: token.tokenId,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image,
+          mimeType: metadata.mime_type,
+          encryptLogic: metadata.encrypt_logic,
+          address: token.contractAddress,
+        };
+      })
+    );
+    return items;
+  };
+
+  listMintedNFTMetadata = async (userAddress: string): Promise<(ExtendedMetadeta | undefined)[]> => {
+    if (!this.dataCollector) throw new Error("please set dataCollectorAddress");
+    const tokens = await this.dataCollector.methods.getAllMintedTokensOfNFT(userAddress).call();
+    const items: (ExtendedMetadeta | undefined)[] = await Promise.all(
+      tokens.map(async (token: TokenInfo) => {
+        const metadata: PlainMetadata = (
+          await axios.get(token.tokenURI, { validateStatus: (status) => status === 200 })
+        ).data;
+        return {
+          id: token.tokenId,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image,
+          mimeType: metadata.mime_type,
+          encryptLogic: metadata.encrypt_logic,
+          address: token.contractAddress,
+        };
+      })
+    );
+    return items;
+  };
+
+  listMintedERC1155Metadata = async (userAddress: string): Promise<(ExtendedMetadeta | undefined)[]> => {
+    if (!this.dataCollector) throw new Error("please set dataCollectorAddress");
+    const tokens = await this.dataCollector.methods.getAllMintedTokensOfERC1155(userAddress).call();
+    const items: (ExtendedMetadeta | undefined)[] = await Promise.all(
+      tokens.map(async (token: TokenInfo) => {
+        const metadata: PlainMetadata = (
+          await axios.get(token.tokenURI, { validateStatus: (status) => status === 200 })
+        ).data;
+        return {
+          id: token.tokenId,
+          name: metadata.name,
+          description: metadata.description,
+          image: metadata.image,
+          mimeType: metadata.mime_type,
+          encryptLogic: metadata.encrypt_logic,
+          address: token.contractAddress,
+        };
+      })
+    );
+    return items;
+  };
+
   extractMetadata = async (
     contractAddress: string,
     tokenId: number,
