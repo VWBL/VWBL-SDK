de066cc1d251ae3fe4b870a1dff7a96ccefa6979
Taki <hashimoto19980924@gmail.com>
2023-01-12 17:48:35 +0900

    chore: format

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 const path = require('path');
 
-  module.exports = {
-    entry: './src/index.ts',
-   devtool: 'inline-source-map',
-   target: 'node',
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: [ '.tsx', '.ts', '.js' ],
-    },
-    output: {
-      filename: 'index.min.js',
-      path: path.resolve(__dirname, '.'),
-    },
-  };
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.ts', '.js' ],
+  },
+  output: {
+    filename: 'index.min.js',
+    path: path.resolve(__dirname, '.'),
+  },
+};
