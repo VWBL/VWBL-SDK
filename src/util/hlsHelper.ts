689b46e222132d43fd168bb645f42f18c951531f
seki <schokolade0317@gmail.com>
2023-04-07 15:42:44 +0900

    lint

--- src/util/hlsHelper.ts
+++ src/util/hlsHelper.ts
@@ -1,33 +1,45 @@
-import * as util from "util";
 import ffmpeg from "fluent-ffmpeg";
+import * as fs from "fs";
+import * as util from "util";
 
-export const makeEncryptedHls = async (filePath: string, duration: number, key: string, iv?: string, outFileName?: string) => {
-    const command = ffmpeg(filePath);
-    console.log('make hls start');
-    command.addOption([
+export const makeEncryptedHls = (
+  filePath: string,
+  duration: number,
+  key: string,
+  iv?: string,
+  outFileName?: string
+) => {
+  const command = ffmpeg(filePath);
+  console.log("make hls start");
+  command
+    .addOption([
       `-hls_enc 1`, // 1 = enable enc
       `-hls_enc_key ${key}`,
       `-hls_enc_iv ${iv || "0".repeat(32)}`,
-      '-profile:v baseline',
-      '-level 3.0',
-      '-start_number 0',
+      `-profile:v baseline`,
+      `-level 3.0`,
+      `-start_number 0`,
       `-hls_time ${duration}`,
-      '-hls_list_size 0',
-      '-f hls'
-    ]).output(outFileName || 'output.m3u8').on('end', (stdout) => {
+      `-hls_list_size 0`,
+      `-f hls`,
+    ])
+    .output(outFileName || "output.m3u8")
+    .on("end", (stdout) => {
       console.log(stdout);
-      console.log('make hls end');
-    }).run();
-  };
+      console.log("make hls end");
+      fs.unlinkSync(`${outFileName || "output.m3u8"}`);
+    })
+    .run();
+};
 
 export const calcSplitNumber = async (path: string, duration: number): Promise<number> => {
   const promise = util.promisify(ffmpeg.ffprobe);
-  const metadata = await promise(path) as any;
+  const metadata = (await promise(path)) as any;
   return Math.ceil(metadata.format.duration / duration);
 };
 
 export const calcDuration = async (path: string, splitNumber: number): Promise<number> => {
   const promise = util.promisify(ffmpeg.ffprobe);
-  const metadata = await promise(path) as any;
+  const metadata = (await promise(path)) as any;
   return Math.ceil(metadata.format.duration / splitNumber);
 };
