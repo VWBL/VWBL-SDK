6b88f35a5740f2fff9d6cc179bf20d5f7516e6de
Taki <hashimoto19980924@gmail.com>
2023-05-10 16:58:58 +0900

    feat: split key

--- src/vwbl/api/VWBLApi.ts
+++ src/vwbl/api/VWBLApi.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { encrypt, decrypt, PrivateKey, PublicKey } from 'eciesjs';
+import * as secrets from "secrets.js-grempe";
 
 export class VWBLApi {
   private instance;
@@ -11,25 +13,33 @@ export class VWBLApi {
     key: string,
     signature: string,
     address?: string,
-    hasNonce?: boolean,
     autoMigration?: boolean
   ) {
-    await this.instance.post("/keys", {
-      document_id: documentId,
-      chain_id: chainId,
-      key,
-      signature,
-      address,
-      has_nonce: hasNonce,
-      auto_migration: autoMigration,
-    });
+    const validatorInfo = (await this.instance.get("/api/v1/validator_info").catch(() => undefined))?.data;
+    const shares = secrets.share(secrets.str2hex(key), validatorInfo.m, validatorInfo.n)
+    const keyMapping: { [key: string]: string } = {};
+    for (let i = 0; i < validatorInfo.m; i++) {
+      const pubKey = new PublicKey(Buffer.from(validatorInfo.publicKeys[i], 'hex'));
+      const encryptedShare = encrypt(pubKey.toHex(), Buffer.from(shares[i]));
+      keyMapping[validatorInfo.publicKeys[i]] = encryptedShare.toString('hex');
+    }
+    await this.instance.post("/api/v1/keys", {
+      userSig: signature,
+      documentId,
+      chainId,
+      autoMigration,
+      keyMapping
+    })
   }
 
   async getKey(documentId: string, chainId: number, signature: string, address?: string): Promise<string> {
-    const response = await this.instance.get(
-      `/keys/${documentId}/${chainId}?signature=${signature}&address=${address}`
-    );
-    return response.data.documentKey.key;
+    const privKey = new PrivateKey();
+    const userPubKey = "0x" + privKey.publicKey.toHex();
+    const encryptedKeys = (await this.instance.get(
+        `/api/v1/keys/${documentId}/${chainId}?userSig=${signature}&userPubkey=${userPubKey}`
+    )).data.encryptedKeys;
+    const keys = encryptedKeys.map((k: string) => decrypt(privKey.toHex(), Buffer.from(k, 'hex')).toString());
+    return secrets.hex2str(secrets.combine(keys))
   }
 
   async getSignMessage(contractAddress: string, chainId: number, address?: string): Promise<string> {
