2306b3c8c097d556ab0220f1691976147fd431b8
kyok01 <kazutakayokoils@gmail.com>
2023-05-12 17:03:33 +0900

    large test with ethers.js

--- test/large/vwbl/VWBL.test.ts
+++ test/large/vwbl/VWBL.test.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import * as dotenv from "dotenv";
 import sinon from "sinon";
 import Web3 from "web3";
+import { ethers } from "ethers";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const FileAPI = require("file-api"),
   File = FileAPI.File;
@@ -30,14 +31,19 @@ type GasInfo = {
   blockNumber: number
 }
 
+const providerUrl = "https://rpc-mumbai.maticvigil.com/"
+// preparation for web3.js
 const hdWalletProvider = new HDWalletProvider({
   privateKeys: [process.env.PRIVATE_KEY as string],
-  providerOrUrl: "https://rpc-mumbai.maticvigil.com/"
+  providerOrUrl: providerUrl
 })
-
 const web3 = new Web3(hdWalletProvider as provider);
+// preparation for ethers.js
+const privateKey = process.env.PRIVATE_KEY as string;
+const ethProvider = new ethers.providers.JsonRpcProvider(providerUrl);
+const ethSigner = new ethers.Wallet(privateKey, ethProvider);
 
-describe("VWBL", () => {
+describe("VWBL with web3.js", () => {
   const vwbl = new VWBL({
     ipfsNftStorageKey: process.env.NFT_STORAGE_KEY,
     awsConfig: undefined,
@@ -108,6 +114,81 @@ describe("VWBL", () => {
     );
     console.log(tokenId, typeof tokenId);
     expect(typeof tokenId).equal("string"); //WARNING:The return value type for 'tokenId' is a string.
+  });
+});
+
+describe("VWBL with ethers.js", () => {
+  const vwbl = new VWBL({
+    ipfsNftStorageKey: process.env.NFT_STORAGE_KEY,
+    awsConfig: undefined,
+    contractAddress: "0x5af2D607242f604C8f5e04e8B648741EE59ac847",
+    manageKeyType: ManageKeyType.VWBL_NETWORK_SERVER,
+    uploadContentType: UploadContentType.IPFS,
+    uploadMetadataType: UploadMetadataType.IPFS,
+    vwblNetworkUrl: "https://dev.vwbl.network/",
+    ethersProvider: ethProvider,
+    ethersSigner: ethSigner,
+  });
+
+  const testSubscriber = {
+    kickStep: () => {},
+  };
+
+  it("mint token with gas settings", async () => {
+    await vwbl.sign();
+
+    const gasInfo = await fetchGasInfo();
+    if(!gasInfo){
+      throw Error('failed to fetch gas information about polygon')
+    }
+    console.log(gasInfo.standard);
+    const maxPriorityFee_wei = Number(web3.utils.toWei(String(gasInfo.standard.maxPriorityFee.toFixed(9)), 'gwei'));
+    const maxFee_wei = Number(web3.utils.toWei(String(gasInfo.standard.maxFee.toFixed(9)), 'gwei'));
+
+    const tokenId = await vwbl.managedCreateTokenForIPFS(
+      "test token",
+      "test",
+      new File({
+        name: "thumbnail image",
+        type: "image/png",
+        buffer: Buffer.alloc(100),
+      }),
+      new File({
+        name: "plain data",
+        type: "image/png",
+        buffer: Buffer.alloc(100),
+      }),
+      10,
+      "base64",
+      testSubscriber,
+      maxPriorityFee_wei,
+      maxFee_wei
+    );
+    console.log(tokenId, typeof tokenId);
+    expect(typeof tokenId).equal("number");
+  });
+
+  it("mint token without gas settings", async () => {
+    await vwbl.sign();
+
+    const tokenId = await vwbl.managedCreateTokenForIPFS(
+      "test token",
+      "test",
+      new File({
+        name: "thumbnail image",
+        type: "image/png",
+        buffer: Buffer.alloc(100),
+      }),
+      new File({
+        name: "plain data",
+        type: "image/png",
+        buffer: Buffer.alloc(100),
+      }),
+      10,
+      "base64",
+    );
+    console.log(tokenId, typeof tokenId);
+    expect(typeof tokenId).equal("number");
   });
 });
 
