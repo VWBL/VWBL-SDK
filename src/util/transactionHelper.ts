aa073cdab09c7431688b6fd43144ce5a5a524845
kyok01 <kazutakayokoils@gmail.com>
2023-05-24 11:20:16 +0900

    update: make getFeeSettings......() immutable

--- src/util/transactionHelper.ts
+++ src/util/transactionHelper.ts
@@ -2,11 +2,6 @@ export const getFeeSettingsBasedOnEnvironment = (
   maxPriorityFeePerGas: number | undefined,
   maxFeePerGas: number | undefined
 ) => {
-  let _maxPriorityFeePerGas = null;
-  let _maxFeePerGas = null;
-  if (typeof window === "undefined") {
-    _maxPriorityFeePerGas = maxPriorityFeePerGas ? maxPriorityFeePerGas : undefined;
-    _maxFeePerGas = maxFeePerGas ? maxFeePerGas : undefined;
-  }
-  return { maxPriorityFeePerGas: _maxPriorityFeePerGas, maxFeePerGas: _maxFeePerGas };
+  const isRunningOnNode = typeof window === "undefined";
+  return isRunningOnNode ? { maxPriorityFeePerGas, maxFeePerGas } : { maxPriorityFeePerGas: null, maxFeePerGas: null };
 };
