0470549214f5c6162808b90a2ed32bfe0d49dc6e
Taki <hashimoto19980924@gmail.com>
2024-05-30 16:59:51 +0900

    chore: fix package

---
+++ packages/core/.eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:@typescript-eslint/recommended-requiring-type-checking",
+      "plugin:prettier/recommended",
+    ],
+    plugins: ["@typescript-eslint", "simple-import-sort"],
+    parser: "@typescript-eslint/parser",
+    globals: {
+      Atomics: "readonly",
+      SharedArrayBuffer: "readonly",
+    },
+    parserOptions: {
+      sourceType: "module",
+      project: "./tsconfig.json",
+      tsconfigRootDir: "./",
+    },
+    rules: {
+      eqeqeq: ["error", "always", { null: "ignore" }],
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "es5",
+          singleQuote: false,
+          semi: true,
+          printWidth: 120,
+          arrowParens: "always",
+        },
+      ],
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [["\\u0000"], ["^@?\\w"], ["^[^.]"], ["^\\."]],
+        },
+      ],
+      "@typescript-eslint/no-misused-promises": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-floating-promises": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/await-thenable": "warn"
+    },
+};
