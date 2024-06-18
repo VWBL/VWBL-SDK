32096d3351550abf7ef1da5d64ef5f552f61f06b
Tatsuki <hashimoto19980924@gmail.com>
2024-06-18 16:15:12 +0900

    feat: export core types in vwbl-sdk

--- packages/evm/src/index.ts
+++ packages/evm/src/index.ts
@@ -1,7 +1,26 @@
 export * from "./vwbl";
 export * from "./util";
+
+export { 
+  AWSConfig,
+  IPFSConfig,
+  PlainMetadata,
+  Metadata,
+  ExtractMetadata,
+  ExtendedMetadeta,
+  EncryptLogic,
+  UploadEncryptedFile,
+  UploadThumbnail,
+  UploadMetadata,
+  UploadEncryptedFileToIPFS,
+  UploadThumbnailToIPFS,
+  UploadMetadataToIPFS,
+  FileOrPath,
+  Base64DataUrl,
+  ManageKeyType,
+  StepStatus,
+  ProgressSubscriber,
+  UploadContentType,
+  UploadMetadataType
+} from "vwbl-core";
 export * as core from "vwbl-core";
-
-export { ManageKeyType } from "vwbl-core";
-
-
